56736eb177edf67d1f73584f1511d9920bc6ae45
ELX571 <20saidazimxon07@gmail.com>
2026-06-28 05:48:58 +0500

    Update migration dependencies for chat and buildChange chat.0002 dependency to build.0002_builderprofile

--- chat/migrations/0002_alter_chatroom_options_alter_message_options_and_more.py
+++ chat/migrations/0002_alter_chatroom_options_alter_message_options_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('build', '0001_initial'),
+        ('build', '0002_builderprofile'),
         ('chat', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
