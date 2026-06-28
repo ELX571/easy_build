da0cd70904ad4b3f4690d7a5bab64a048d67e5ad
ELX571 <20saidazimxon07@gmail.com>
2026-06-28 05:56:12 +0500

    Update build.sh to manage migration modulesFix chicken-and-egg migration problem in build.sh
    Add temporary setting for migration modules during build.

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+echo "MIGRATION_MODULES = {'chat': None}" >> conf/settings.py
 python manage.py makemigrations build --name builderprofile
+sed -i "/MIGRATION_MODULES = {'chat': None}/d" conf/settings.py
 python manage.py migrate
