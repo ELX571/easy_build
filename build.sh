c0b0d4c2296cc29fd5e920e12d6295da5d1e7762
ELX571 <20saidazimxon07@gmail.com>
2026-06-28 06:01:27 +0500

    Create superuser in build scriptAdd django superuser script to build.sh
    Add command to create superuser if it doesn't exist

--- build.sh
+++ build.sh
@@ -10,3 +10,4 @@ echo "MIGRATION_MODULES = {'chat': None}" >> conf/settings.py
 python manage.py makemigrations build --name builderprofile
 sed -i "/MIGRATION_MODULES = {'chat': None}/d" conf/settings.py
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass') if not User.objects.filter(username='admin').exists() else None"
