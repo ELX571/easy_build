ce43c7ebd8c1493a597f33e7c8ea8177c80a367b
ELX571 <20saidazimxon07@gmail.com>
2026-07-06 12:22:09 +0500

    Update logo to EasyBuild.uz and add keep_alive

--- build/apps.py
+++ build/apps.py
@@ -11,5 +11,5 @@ class BuildConfig(AppConfig):
         # Prevent running thread twice in dev (runserver starts 2 processes by default)
         if os.environ.get('RUN_MAIN', None) == 'true' or not settings.DEBUG:
             from .keep_alive import start_keep_alive
-            # Production domain
-            start_keep_alive('https://easybuild.uz/ping/')
+            # Render URL to keep the app awake
+            start_keep_alive('https://easy-build-bmz2.onrender.com/ping/')
